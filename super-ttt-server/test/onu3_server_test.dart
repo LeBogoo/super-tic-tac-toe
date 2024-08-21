5f5eae9ddfb2200477cab3ce2b5e8370ce1bae73
LeBogo <lebogomc@gmail.com>
2024-08-21 22:50:53 +0200

    remove onu3 server test

--- super-ttt-server/test/onu3_server_test.dart
+++
@@ -1,8 +0,0 @@
-// import 'package:super_ttt_server/super_ttt_server.dart';
-// import 'package:test/test.dart';
-
-void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
-}
